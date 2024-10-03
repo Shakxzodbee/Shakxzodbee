eda188fc3ba0b069ffb8a70f994110648dbfda4e
Shahzodbek <136874725+Shakxzodbee@users.noreply.github.com>
2024-10-03 09:48:05 +0500

    Update Info.js

---
+++ Info.js
@@ -0,0 +1,12 @@
+const Shakhzodbek = {
+    name: ["Shakhzodbek"],
+    AskMeAbout: ["frontend dev", "Backend dev", "React.js dev"],
+    langues: ["english - Pre Intermediate"] ,
+    degre: ["Middle "],
+    technologies:{
+        FrontEnd: ["Html5", "CSS3",  "JavaScript" ,"TypeScript", "React.js", "Next.js"],
+        BackEnd: ["Node.js" , "MongoDb" , "Express.js" , "Handlebars"],
+        style Library: ["Bootstrap 4", "Tailwindcss", "Material UI"],
+        misc: ["Firebase"],
+        age: ["17"]
+    }
